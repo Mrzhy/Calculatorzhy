7c740a9d6403362eecd9631fbda204d2e90a450f
Mr.Zhao <mister.zhao@foxmail.com>
2016-03-28 21:19:33 +0800

    实现点按求值
    实现点按求值

--- Calculatorzhy/ViewController.swift
+++ Calculatorzhy/ViewController.swift
@@ -9,7 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var 计算器 = 计算()
+    var 显示缓存 = ""
 
+    @IBOutlet weak var 屏幕: UITextField!
     
+    @IBAction func 数字9(sender: AnyObject) {
+        点按("9")
+    }
+    @IBAction func 数字8(sender: AnyObject) {
+        点按("8")
+    }
+    @IBAction func 数字7(sender: AnyObject) {
+        点按("7")
+    }
+    @IBAction func 数字6(sender: AnyObject) {
+        点按("6")
+    }
+    @IBAction func 数字5(sender: AnyObject) {
+        点按("5")
+    }
+    @IBAction func 数字4(sender: AnyObject) {
+        点按("4")
+    }
+    @IBAction func 数字3(sender: AnyObject) {
+        点按("3")
+    }
+    @IBAction func 数字2(sender: AnyObject) {
+        点按("2")
+    }
+    @IBAction func 数字1(sender: AnyObject) {
+        点按("1")
+    }
+    @IBAction func 数字0(sender: AnyObject) {
+        点按("0")
+    }
+    @IBAction func 数字点(sender: AnyObject) {
+        点按(".")
+    }
+    @IBAction func 算法加(sender: AnyObject) {
+        计算器.设置当前算法(.加法)
+        求值()
+    }
+    @IBAction func 算法减(sender: AnyObject) {
+        计算器.设置当前算法(.减法)
+        求值()
+    }
+    @IBAction func 算法乘(sender: AnyObject) {
+        计算器.设置当前算法(.乘法)
+        求值()
+    }
+    @IBAction func 算法除(sender: AnyObject) {
+        计算器.设置当前算法(.除法)
+        求值()
+    }
+    @IBAction func 算法百分比(sender: AnyObject) {
+        计算器.设置当前算法(.百分号)
+        求值()
+    }
+    @IBAction func 重置当前(sender: AnyObject) {
+    }
+    @IBAction func 重置所有(sender: AnyObject) {
+    }
+    @IBAction func 结果(sender: AnyObject) {
+        求值()
+    }
+    
+    func 点按(数字:String) {
+        显示缓存 += 数字
+        //屏幕.stringValue
+    }
+    
+    func 求值() {
+        
+    }
 }
 
---
+++ Calculatorzhy/计算.swift
@@ -0,0 +1,52 @@
+//
+//  计算.swift
+//  Calculatorzhy
+//
+//  Created by Mr.Zhao on 16/3/28.
+//  Copyright © 2016年 Mr.Zhao. All rights reserved.
+//
+
+import Foundation
+
+struct 计算 {
+    private var 当前算法:算法 = .未选择
+    
+    mutating func 设置当前算法(临时:算法) {
+        当前算法 = 临时
+    }
+    
+    func 读取当前算法() -> 算法 {
+        return 当前算法
+    }
+    
+    enum 算法 {
+        case 加法
+        case 减法
+        case 乘法
+        case 除法
+        case 百分号
+        case 未选择
+    }
+    
+    func 求结果(操作数 操作数:Double, 被操作数:Double) ->String {
+        var 返回 = ""
+        switch 当前算法 {
+        case .加法:
+            返回 = "\(操作数 + 被操作数)"
+        case .减法:
+            返回 = "\(操作数 - 被操作数)"
+        case .乘法:
+            返回 = "\(操作数 * 被操作数)"
+        case .除法 where 被操作数 != 0 :
+            返回 = "\(操作数 / 被操作数)"
+        case .百分号:
+            返回 = "\(操作数 / 100)"
+        case .未选择:
+            返回 = "错误：未选择算法"
+        default:
+            返回 = "0 不能做被除数！"
+            
+        }
+        return 返回
+    }
+}
