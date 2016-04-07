0d4b68cd3c79305a82242602825adfeb0353dfb2
Mr.Zhao <mister.zhao@foxmail.com>
2016-04-07 12:18:06 +0800

    初步规范代码1
    初步规范代码1

--- Calculatorzhy/ViewController.swift
+++ Calculatorzhy/ViewController.swift
@@ -5,35 +5,38 @@
 //  Created by Mr.Zhao on 16/3/21.
 //  Copyright © 2016年 Mr.Zhao. All rights reserved.
 //
+/*
+计算器：Calculator
+显示缓存：Displaycache
+缓存：cache
+屏幕：screen
+temporary：临时
+数字：number
+
+
+
+
+
+@IBAction func 数字9(sender: AnyObject) {
+点按("9")
+}
+
+
+
+
+*/
 
 import UIKit
 
 class ViewController: UIViewController {
-    var 计算器 = 计算()
-    var 显示缓存 = ""
-    var 缓存 = 0.0
-    var 缓存2:Double?
+    var Calculator = calculation()
+    var Displaycache = ""
+    var cache = 0.0
+    var cache2:Double?
 
-    @IBOutlet weak var 屏幕: UITextField!
+    @IBOutlet weak var screen: UITextField!
     
-    @IBAction func 数字9(sender: AnyObject) {
-        点按("9")
-    }
-    @IBAction func 数字8(sender: AnyObject) {
-        点按("8")
-    }
-    @IBAction func 数字7(sender: AnyObject) {
-        点按("7")
-    }
-    @IBAction func 数字6(sender: AnyObject) {
-        点按("6")
-    }
-    @IBAction func 数字5(sender: AnyObject) {
-        点按("5")
-    }
-    @IBAction func 数字4(sender: AnyObject) {
-        点按("4")
-    }
+    
     @IBAction func 数字3(sender: AnyObject) {
         点按("3")
     }
@@ -50,62 +53,62 @@ class ViewController: UIViewController {
         点按(".")
     }
     @IBAction func 算法加(sender: AnyObject) {
-        if 计算器.读取当前算法() != .加法 {//如果点击加之前是别的算法就执行这里的代码块
+        if Calculator.Readthecurrentalgorithm() != .addition {//如果点击加之前是别的算法就执行这里的代码块
             求值()
-            计算器.设置当前算法(.加法)
+            Calculator.Setcurrentalgorithm(.addition)
             return //用return语句让函数提前出栈
         }
-        计算器.设置当前算法(.加法)
+        Calculator.Setcurrentalgorithm(.addition)
         求值()
     }
     @IBAction func 算法减(sender: AnyObject) {
-        if 计算器.读取当前算法() != .减法 {
+        if Calculator.Readthecurrentalgorithm() != .subtraction {
             求值()
-            计算器.设置当前算法(.减法)
+            Calculator.Setcurrentalgorithm(.subtraction)
             return
         }
-        计算器.设置当前算法(.减法)
+        Calculator.Setcurrentalgorithm(.subtraction)
         求值()
     }
     @IBAction func 算法乘(sender: AnyObject) {
-        if 计算器.读取当前算法() != .乘法 {
+        if Calculator.Readthecurrentalgorithm() != .multiplication {
             求值()
-            计算器.设置当前算法(.乘法)
+            Calculator.Setcurrentalgorithm(.multiplication)
             return
         }
-        计算器.设置当前算法(.乘法)
+        Calculator.Setcurrentalgorithm(.multiplication)
         求值()
     }
     @IBAction func 算法除(sender: AnyObject) {
-        if 计算器.读取当前算法() != .除法 {
+        if Calculator.Readthecurrentalgorithm() != .division {
             求值()
-            计算器.设置当前算法(.除法)
+            Calculator.Setcurrentalgorithm(.division)
             return
         }
-        计算器.设置当前算法(.除法)
+        Calculator.Setcurrentalgorithm(.division)
         求值()
     }
     @IBAction func 算法百分比(sender: AnyObject) {
-        if 计算器.读取当前算法() != .百分号 {
+        if Calculator.Readthecurrentalgorithm() != .Percent {
             求值()
-            计算器.设置当前算法(.百分号)
+            Calculator.Setcurrentalgorithm(.Percent)
             return
         }
-        计算器.设置当前算法(.百分号)
+        Calculator.Setcurrentalgorithm(.Percent)
         求值()
     }
     @IBAction func 重置当前(sender: AnyObject) {
         点计数 = 0
-        显示缓存 = ""
-        屏幕.text = ""
+        Displaycache = ""
+        screen.text = ""
     }
     @IBAction func 重置所有(sender: AnyObject) {
         点计数 = 0 //同理，清空重置什么的，都要加上这个
-        显示缓存 = ""
-        缓存 = 0.0
-        缓存2 = nil
-        计算器.设置当前算法(.未选择)
-        屏幕.text = ""
+        Displaycache = ""
+        cache = 0.0
+        cache2 = nil
+        Calculator.Setcurrentalgorithm(.Nochoice)
+        screen.text = ""
     }
     @IBAction func 结果(sender: AnyObject) {
         求值()
@@ -119,30 +122,30 @@ class ViewController: UIViewController {
         if 数字 == "." && 点计数 > 1 { //如果已经输入了一个小数点，还要输入小数点就略过后面的代码，也就不会计入缓存
             return //函数遇到return就会出栈了
         }
-        显示缓存 += 数字
-        屏幕.text = 显示缓存
+        Displaycache += 数字
+        screen.text = Displaycache
     }
     
     func 求值() {
         点计数 = 0 //这里我们清空统计，因为你按了等于不是吗？这样就又可以输入小数点了
-        if !显示缓存.isEmpty {
-            let 临时 = 显示缓存 as NSString
-            缓存 = 临时.doubleValue
-            显示缓存 = ""
+        if !Displaycache.isEmpty {
+            let 临时 = Displaycache as NSString
+            cache = 临时.doubleValue
+            Displaycache = ""
         }
-        let 临时 = 缓存
+        let 临时 = cache
         var 结果 = ""
         
-        if  let 前一个数字 = 缓存2 {
+        if  let 前一个数字 = cache2 {
         
-            结果 = 计算器.求结果(操作数: 前一个数字, 被操作数: 临时)
-            屏幕.text = 结果
+            结果 = Calculator.Resultseeking(Operationnumber: 前一个数字, Operandoperand: 临时)
+            screen.text = 结果
             let 临 = 结果 as NSString
-            缓存2 = 临.doubleValue
+            cache2 = 临.doubleValue
             
         } else {
-            缓存2 = 缓存
-            缓存 = 0.0
+            cache2 = cache
+            cache = 0.0
         }
     }
 }
--- Calculatorzhy/计算.swift
+++ Calculatorzhy/计算.swift
@@ -5,48 +5,66 @@
 //  Created by Mr.Zhao on 16/3/28.
 //  Copyright © 2016年 Mr.Zhao. All rights reserved.
 //
+/*
+calculation：计算
+Currentalgorithm:当前算法
+algorithm：算法
+Nochoice：未选择
+Setcurrentalgorithm：设置当前算法
+temporary：临时
+Readthecurrentalgorithm：读取当前算法
+addition：加法
+subtraction：减法
+multiplication：乘法
+division：除法
+Percent：百分号
+Resultseeking：求结果
+Operationnumber：操作数
+Operandoperand：被操作数
+revert：返回
+*/
 
 import Foundation
 
-struct 计算 {
-    private var 当前算法:算法 = .未选择
+struct calculation {
+    private var Currentalgorithm:algorithm = .Nochoice
     
-    mutating func 设置当前算法(临时:算法) {
-        当前算法 = 临时
+    mutating func Setcurrentalgorithm(temporary:algorithm) {
+        Currentalgorithm = temporary
     }
     
-    func 读取当前算法() -> 算法 {
-        return 当前算法
+    func Readthecurrentalgorithm() -> algorithm {
+        return Currentalgorithm
     }
     
-    enum 算法 {
-        case 加法
-        case 减法
-        case 乘法
-        case 除法
-        case 百分号
-        case 未选择
+    enum algorithm {
+        case addition
+        case subtraction
+        case multiplication
+        case division
+        case Percent
+        case Nochoice
     }
     
-    func 求结果(操作数 操作数:Double, 被操作数:Double) ->String {
-        var 返回 = ""
-        switch 当前算法 {
-        case .加法:
-            返回 = "\(操作数 + 被操作数)"
-        case .减法:
-            返回 = "\(操作数 - 被操作数)"
-        case .乘法:
-            返回 = "\(操作数 * 被操作数)"
-        case .除法 where 被操作数 != 0 :
-            返回 = "\(操作数 / 被操作数)"
-        case .百分号:
-            返回 = "\(操作数 / 100)"
-        case .未选择:
-            返回 = "错误：未选择算法"
+    func Resultseeking(Operationnumber Operationnumber:Double, Operandoperand:Double) ->String {
+        var revert = ""
+        switch Currentalgorithm {
+        case .addition:
+            revert = "\(Operationnumber + Operandoperand)"
+        case .subtraction:
+            revert = "\(Operationnumber - Operandoperand)"
+        case .multiplication:
+            revert = "\(Operationnumber * Operandoperand)"
+        case .division where Operandoperand != 0 :
+            revert = "\(Operationnumber / Operandoperand)"
+        case .Percent:
+            revert = "\(Operationnumber / 100)"
+        case .Nochoice:
+            revert = "错误：未选择算法"
         default:
-            返回 = "0 不能做被除数！"
+            revert = "0 不能做被除数！"
             
         }
-        return 返回
+        return revert
     }
 }
