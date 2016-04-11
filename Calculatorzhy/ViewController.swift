97688d6207810fc40cec21b02d2a2a470a37e56b
Mr.Zhao <mister.zhao@foxmail.com>
2016-04-11 20:23:43 +0800

    规范代码、修改bug、添加app图标、增加欢迎页
    规范代码、修改bug、添加app图标、增加欢迎页

--- Calculatorzhy/ViewController.swift
+++ Calculatorzhy/ViewController.swift
@@ -6,24 +6,26 @@
 //  Copyright © 2016年 Mr.Zhao. All rights reserved.
 //
 /*
-计算器：Calculator
-显示缓存：Displaycache
-缓存：cache
+计算器:Calculator
+显示缓存:Displaycache
+缓存:cache
+求值:evaluation
+点计数:Pointcount
+点按:click
+数字:number
+前一个数字:Lastnumber
+临时:temporary
+结果:result
 屏幕：screen
-temporary：临时
-数字：number
-
-
-
-
-
-@IBAction func 数字9(sender: AnyObject) {
-点按("9")
-}
-
-
-
-
+小数点：point
+加号：plussign
+减号：minussign
+乘号：multiplicationsign
+除号：divisionsign
+百分号：percent
+重置当前：Resetcurrent
+重置所有：Resetall
+等号：equalsign
 */
 
 import UIKit
@@ -36,112 +38,135 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var screen: UITextField!
     
-    
-    @IBAction func 数字3(sender: AnyObject) {
-        点按("3")
+    @IBAction func number9(sender: AnyObject) {
+        click("9")
     }
-    @IBAction func 数字2(sender: AnyObject) {
-        点按("2")
+    @IBAction func number8(sender: AnyObject) {
+        click("8")
     }
-    @IBAction func 数字1(sender: AnyObject) {
-        点按("1")
+    @IBAction func number7(sender: AnyObject) {
+        click("7")
     }
-    @IBAction func 数字0(sender: AnyObject) {
-        点按("0")
+    @IBAction func number6(sender: AnyObject) {
+        click("6")
     }
-    @IBAction func 数字点(sender: AnyObject) {
-        点按(".")
+    @IBAction func number5(sender: AnyObject) {
+        click("5")
     }
-    @IBAction func 算法加(sender: AnyObject) {
+    @IBAction func number4(sender: AnyObject) {
+        click("4")
+    }
+    @IBAction func number3(sender: AnyObject) {
+        click("3")
+    }
+    @IBAction func number2(sender: AnyObject) {
+        click("2")
+    }
+    @IBAction func number1(sender: AnyObject) {
+        click("1")
+    }
+    @IBAction func number0(sender: AnyObject) {
+        if screen.text == "0"
+        {
+            screen.text = screen.text! + ""
+        }
+        else {
+            click("0")
+        }
+    }
+    @IBAction func point(sender: AnyObject) {
+        click(".")
+    }
+    @IBAction func plussign(sender: AnyObject) {
         if Calculator.Readthecurrentalgorithm() != .addition {//如果点击加之前是别的算法就执行这里的代码块
-            求值()
+            evaluation()
             Calculator.Setcurrentalgorithm(.addition)
             return //用return语句让函数提前出栈
         }
         Calculator.Setcurrentalgorithm(.addition)
-        求值()
+        evaluation()
     }
-    @IBAction func 算法减(sender: AnyObject) {
+    @IBAction func minussign(sender: AnyObject) {
         if Calculator.Readthecurrentalgorithm() != .subtraction {
-            求值()
+            evaluation()
             Calculator.Setcurrentalgorithm(.subtraction)
             return
         }
         Calculator.Setcurrentalgorithm(.subtraction)
-        求值()
+        evaluation()
     }
-    @IBAction func 算法乘(sender: AnyObject) {
+    @IBAction func multiplicationsign(sender: AnyObject) {
         if Calculator.Readthecurrentalgorithm() != .multiplication {
-            求值()
+            evaluation()
             Calculator.Setcurrentalgorithm(.multiplication)
             return
         }
         Calculator.Setcurrentalgorithm(.multiplication)
-        求值()
+        evaluation()
     }
-    @IBAction func 算法除(sender: AnyObject) {
+    @IBAction func divisionsign(sender: AnyObject) {
         if Calculator.Readthecurrentalgorithm() != .division {
-            求值()
+            evaluation()
             Calculator.Setcurrentalgorithm(.division)
             return
         }
         Calculator.Setcurrentalgorithm(.division)
-        求值()
+        evaluation()
     }
-    @IBAction func 算法百分比(sender: AnyObject) {
+    @IBAction func percent(sender: AnyObject) {
         if Calculator.Readthecurrentalgorithm() != .Percent {
-            求值()
+            evaluation()
             Calculator.Setcurrentalgorithm(.Percent)
             return
         }
         Calculator.Setcurrentalgorithm(.Percent)
-        求值()
+        evaluation()
     }
-    @IBAction func 重置当前(sender: AnyObject) {
-        点计数 = 0
+    @IBAction func Resetcurrent(sender: AnyObject) {
+        Pointcount = 0
         Displaycache = ""
         screen.text = ""
     }
-    @IBAction func 重置所有(sender: AnyObject) {
-        点计数 = 0 //同理，清空重置什么的，都要加上这个
+    @IBAction func Resetall(sender: AnyObject) {
+        Pointcount = 0 //同理，清空重置什么的，都要加上这个
         Displaycache = ""
         cache = 0.0
         cache2 = nil
         Calculator.Setcurrentalgorithm(.Nochoice)
         screen.text = ""
     }
-    @IBAction func 结果(sender: AnyObject) {
-        求值()
+    @IBAction func equalsign(sender: AnyObject) {
+        evaluation()
     }
     
-    var 点计数 = 0;
-    func 点按(数字:String) {
-        if 数字 == "." { //如果是小数点，就增加计数
-            点计数++
+    var Pointcount = 0;
+    func click(number:String) {
+        if number == "." { //如果是小数点，就增加计数
+            Pointcount++
         }
-        if 数字 == "." && 点计数 > 1 { //如果已经输入了一个小数点，还要输入小数点就略过后面的代码，也就不会计入缓存
+        if number == "." && Pointcount > 1 { //如果已经输入了一个小数点，还要输入小数点就略过后面的代码，也就不会计入缓存
             return //函数遇到return就会出栈了
         }
-        Displaycache += 数字
+        Displaycache += number
         screen.text = Displaycache
     }
     
-    func 求值() {
-        点计数 = 0 //这里我们清空统计，因为你按了等于不是吗？这样就又可以输入小数点了
+    func evaluation() {
+        Pointcount = 0 //这里我们清空统计，因为你按了等于不是吗？这样就又可以输入小数点了
         if !Displaycache.isEmpty {
-            let 临时 = Displaycache as NSString
-            cache = 临时.doubleValue
+            let temporary = Displaycache as NSString
+            cache = temporary.doubleValue
             Displaycache = ""
         }
-        let 临时 = cache
-        var 结果 = ""
+        let temporary = cache
+        var result = ""
         
-        if  let 前一个数字 = cache2 {
+        if  let Lastnumber = cache2 {
         
-            结果 = Calculator.Resultseeking(Operationnumber: 前一个数字, Operandoperand: 临时)
-            screen.text = 结果
-            let 临 = 结果 as NSString
-            cache2 = 临.doubleValue
+            result = Calculator.Resultseeking(Operationnumber: Lastnumber, Operandoperand: temporary)
+            screen.text = result
+            let temporary2 = result as NSString
+            cache2 = temporary2.doubleValue
             
         } else {
             cache2 = cache
